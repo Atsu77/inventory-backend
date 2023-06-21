33023170ce2a5ff3d85f9745722526aeddcff1d3
Atsu <kamnknon@gmail.com>
2023-06-21 23:01:11 +0900

    feat: ItemRestControllerにfindAll()メソッドを追加

--- src/main/java/com/example/inventory/web/api/ItemRestController.java
+++ src/main/java/com/example/inventory/web/api/ItemRestController.java
@@ -1,2 +1,25 @@
-package com.example.inventory.web.api;public class ItemRestController {
+package com.example.inventory.web.api;
+
+import com.example.inventory.entity.Item;
+import com.example.inventory.service.ItemService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/items")
+public class ItemRestController {
+    private final ItemService itemService;
+
+    public ItemRestController(ItemService itemService) {
+        this.itemService = itemService;
+    }
+
+    @GetMapping
+    public List<Item> findAll() {
+        return itemService.findAll();
+    }
+
 }
