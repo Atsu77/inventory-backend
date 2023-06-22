8f4f0971ad0975941a8289d2ac9962b41c51934f
Atsu <kamnknon@gmail.com>
2023-06-22 10:09:45 +0900

    refactor: ja: ダミーデータの挿入にINSERT IGNOREを使用

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,12 +1,12 @@
 -- itemsテーブルにダミーデータを挿入
-INSERT INTO items (code, name) VALUES
+INSERT IGNORE INTO items (code, name) VALUES
 ('0001', 'Item 1'),
 ('0002', 'Item 2'),
 ('0003', 'Item 3'),
 ('0004', 'Item 4');
 
 -- inventory_transactionsテーブルにダミーデータを挿入
-INSERT INTO inventory_transactions (type, date_time, item_code, quantity, description) VALUES
+INSERT IGNORE INTO inventory_transactions (type, date_time, item_code, quantity, description) VALUES
 ('ADJUST', '2023-06-22 10:00:00', '0001', 10, 'Adjustment 1'),
 ('RECEPT', '2023-06-22 11:00:00', '0002', 5, 'Receipt 1'),
 ('ISSUE', '2023-06-22 12:00:00', '0003', -3, 'Issue 1'),
