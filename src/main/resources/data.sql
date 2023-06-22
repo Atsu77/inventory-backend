efb5a8851f82a6742a615afa9890cc204b826cec
Atsu <kamnknon@gmail.com>
2023-06-22 09:46:19 +0900

    feat: ダミーデータを挿入

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,12 +1,15 @@
-INSERT IGNORE INTO items (code, name)
-VALUES
-('001', 'Item 1'),
-('002', 'Item 2'),
-('003', 'Item 3'),
-('004', 'Item 4'),
-('005', 'Item 5'),
-('006', 'Item 6'),
-('007', 'Item 7'),
-('008', 'Item 8'),
-('009', 'Item 9'),
-('010', 'Item 10');
+-- itemsテーブルにダミーデータを挿入
+INSERT INTO items (code, name) VALUES
+('0001', 'Item 1'),
+('0002', 'Item 2'),
+('0003', 'Item 3'),
+('0004', 'Item 4');
+
+-- inventory_transactionsテーブルにダミーデータを挿入
+INSERT INTO inventory_transactions (type, date_time, item_code, quantity, description) VALUES
+('ADJUST', '2023-06-22 10:00:00', '0001', 10, 'Adjustment 1'),
+('RECEPT', '2023-06-22 11:00:00', '0002', 5, 'Receipt 1'),
+('ISSUE', '2023-06-22 12:00:00', '0003', -3, 'Issue 1'),
+('ADJUST', '2023-06-23 09:00:00', '0004', 8, 'Adjustment 2'),
+('RECEPT', '2023-06-23 10:00:00', '0001', 2, 'Receipt 2'),
+('ISSUE', '2023-06-23 11:00:00', '0002', -4, 'Issue 2');
